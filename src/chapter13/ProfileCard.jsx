7492f42e948df546ec986ba9c175d4f523bc4794
정일규 <wjddlfrb131@naver.com>
2024-01-11 21:27:48 +0900

    Composition, inheritance

---
+++ src/chapter13/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+function ProfileCard(props) {
+	return (
+		<Card title='Inje Lee' backgroundColor='white'>
+			<p>안녕하세요, 일규입니다</p>
+			<p>저는 리액트를 개발하고 있습니다.</p>
+		</Card>
+	)
+	
+}
+
+export default ProfileCard;
--- src/index.js
+++ src/index.js
@@ -11,11 +11,12 @@ import LandingPage from './chapter09/LandingPage';
 import AttendanceBook from './chapter10/AttendanceBook';
 import SignUp from './chapter11/SignUp';
 import Calculator from './chapter12/Calculator';
+import ProfileCard from './chapter13/ProfileCard';
 
 const root = document.getElementById('root');
 const rootElement = (
   <React.StrictMode>
-    <Calculator />
+    <ProfileCard />
   </React.StrictMode>
 );
 
